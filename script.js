33444b0c24d4efc72d72c790903b513d70991778
Thuyeain Htun <thuyeainhtun2001@gmail.com>
2024-02-13 18:27:08 +0630

    portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open")
+}
+// scroll
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
